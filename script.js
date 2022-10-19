40d99d130473c9979eeca8d3c8f17eb69a9417f4
GryRgrs <102528828+GryRgrs@users.noreply.github.com>
2022-10-19 14:37:33 +0100

    update

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let episodeTab,
   episodeSummary,
   searchField,
   allEpisodesDiv;
-<<<<<<< HEAD
 
 searchField = documentElement.appendChild(document.createElement("input"));
 searchField.id = "searchField";
@@ -65,29 +64,3 @@ const searchEpisodes = (episode) => {
   });
 };
 searchEpisodes(allEpisodes);
-=======
-
-searchField = documentElement.appendChild(document.createElement("input"));
-searchField.id = "searchField";
-allEpisodesDiv = documentElement.appendChild(document.createElement("div"));
-allEpisodesDiv.className = "allEpisodesDiv";
-
-searchField.addEventListener("keydown", () => {
-});
-
-allEpisodes.forEach((item) => {
-  episodeTab = allEpisodesDiv.appendChild(document.createElement("section"));
-  episodeName = episodeTab.appendChild(document.createElement("h1"));
-  episodeThumbnail = episodeTab.appendChild(document.createElement("img"));
-  episodeSummary = episodeTab.appendChild(document.createElement("p"));
-  episodeTab.className = "episodeTab";
-  episodeName.className = "episodeName";
-  episodeThumbnail.className = "episodeThumbnail";
-  episodeSummary.className = "episodeSummary";
-  episodeName.innerText = `${item.name} - S${("0" + item.season).slice(-2)}E${(
-    "0" + item.number
-  ).slice(-2)}`;
-  episodeThumbnail.src = item.image.medium;
-  episodeSummary.innerHTML = item.summary;
-});
->>>>>>> d6d84b9013b6a7e5e7c82802933f8408c12a7312

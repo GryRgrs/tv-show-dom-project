d725dbed8738d1d7d461af4e8f9ea0efde44d115
GryRgrs <102528828+GryRgrs@users.noreply.github.com>
2022-10-20 13:06:43 +0100

    latest tinkering

--- script.js
+++ script.js
@@ -12,21 +12,51 @@ searchField.id = "searchField";
 allEpisodesDiv = documentElement.appendChild(document.createElement("div"));
 allEpisodesDiv.className = "allEpisodesDiv";
 
-searchField.addEventListener("keydown", () => {
+fetch("https://api.tvmaze.com/shows/82/episodes")
+.then(function (response) {
+return response.json();
+})
+.then(function (data) {
+allEpisodes = data;
+makePageForEpisodes(allEpisodes);
+console.log(allEpisodes);
+})
+.catch((error) => {
 });
 
-allEpisodes.forEach((item) => {
-  episodeTab = allEpisodesDiv.appendChild(document.createElement("section"));
-  episodeName = episodeTab.appendChild(document.createElement("h1"));
-  episodeThumbnail = episodeTab.appendChild(document.createElement("img"));
-  episodeSummary = episodeTab.appendChild(document.createElement("p"));
-  episodeTab.className = "episodeTab";
-  episodeName.className = "episodeName";
-  episodeThumbnail.className = "episodeThumbnail";
-  episodeSummary.className = "episodeSummary";
-  episodeName.innerText = `${item.name} - S${("0" + item.season).slice(-2)}E${(
-    "0" + item.number
-  ).slice(-2)}`;
-  episodeThumbnail.src = item.image.medium;
-  episodeSummary.innerHTML = item.summary;
-});
+function makePageForEpisodes(episode) {
+  episode.forEach((item) => {
+    episodeTab = allEpisodesDiv.appendChild(document.createElement("section"));
+    episodeName = episodeTab.appendChild(document.createElement("h1"));
+    episodeThumbnail = episodeTab.appendChild(document.createElement("img"));
+    episodeSummary = episodeTab.appendChild(document.createElement("p"));
+    episodeTab.className = "episodeTab";
+    episodeName.className = "episodeName";
+    episodeThumbnail.className = "episodeThumbnail";
+    episodeSummary.className = "episodeSummary";
+    episodeName.innerText = `${item.name} - S${("0" + item.season).slice(
+      -2
+    )}E${("0" + item.number).slice(-2)}`;
+    episodeThumbnail.src = item.image.medium;
+    episodeSummary.innerHTML = item.summary;
+  });
+}
+makePageForEpisodes(allEpisodes);
+
+const searchEpisodes = (episode) => {
+  const input = document.getElementById("searchField");
+  input.addEventListener("input", (event) => {
+    let searchTerm = event.target.value.toLowerCase();
+
+    let filteredEpisodes = episode.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(searchTerm) ||
+        item.summary.toLowerCase().includes(searchTerm)
+      );
+    });
+    allEpisodesDiv.innerHTML = "";
+    console.log(allEpisodesDiv);
+    makePageForEpisodes(filteredEpisodes);
+  });
+};
+searchEpisodes(allEpisodes);
